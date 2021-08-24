ef1cc118814141de91fd37794c140e4ab7595a4c
Craig Raw <craigraw@gmail.com>
2021-08-24 08:44:33 +0200

    add whirlpoolwallet to walletstartevent and walletstopevent

--- src/main/java/com/samourai/whirlpool/client/event/WalletStartEvent.java
+++ src/main/java/com/samourai/whirlpool/client/event/WalletStartEvent.java
@@ -1,14 +1,21 @@
 package com.samourai.whirlpool.client.event;
 
+import com.samourai.whirlpool.client.wallet.WhirlpoolWallet;
 import com.samourai.whirlpool.client.wallet.beans.WhirlpoolEvent;
 import com.samourai.whirlpool.client.wallet.data.utxo.UtxoData;
 
 public class WalletStartEvent extends WhirlpoolEvent {
+  private WhirlpoolWallet whirlpoolWallet;
   private UtxoData utxoData;
 
-  public WalletStartEvent(UtxoData utxoData) {
+  public WalletStartEvent(WhirlpoolWallet whirlpoolWallet, UtxoData utxoData) {
     super();
+    this.whirlpoolWallet = whirlpoolWallet;
     this.utxoData = utxoData;
+  }
+
+  public WhirlpoolWallet getWhirlpoolWallet() {
+    return whirlpoolWallet;
   }
 
   public UtxoData getUtxoData() {
--- src/main/java/com/samourai/whirlpool/client/event/WalletStopEvent.java
+++ src/main/java/com/samourai/whirlpool/client/event/WalletStopEvent.java
@@ -1,9 +1,17 @@
 package com.samourai.whirlpool.client.event;
 
+import com.samourai.whirlpool.client.wallet.WhirlpoolWallet;
 import com.samourai.whirlpool.client.wallet.beans.WhirlpoolEvent;
 
 public class WalletStopEvent extends WhirlpoolEvent {
-  public WalletStopEvent() {
+  private WhirlpoolWallet whirlpoolWallet;
+
+  public WalletStopEvent(WhirlpoolWallet whirlpoolWallet) {
     super();
+    this.whirlpoolWallet = whirlpoolWallet;
+  }
+
+  public WhirlpoolWallet getWhirlpoolWallet() {
+    return whirlpoolWallet;
   }
 }
