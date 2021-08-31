d16796b4d9b2fffe5a77ec869ed46089d4fb200d
Craig Raw <craigraw@gmail.com>
2021-08-31 16:31:52 +0200

    add tor change identity functionality

--- src/main/java/com/sparrowwallet/nightjar/tor/WhirlpoolTorClientService.java
+++ src/main/java/com/sparrowwallet/nightjar/tor/WhirlpoolTorClientService.java
@@ -2,10 +2,19 @@ package com.sparrowwallet.nightjar.tor;
 
 import com.samourai.tor.client.TorClientService;
 
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.security.SecureRandom;
+
 public class WhirlpoolTorClientService extends TorClientService {
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     @Override
     public void changeIdentity() {
-        //Nothing for now
+        Authenticator.setDefault(new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return (new PasswordAuthentication("user", Integer.toString(secureRandom.nextInt()).toCharArray()));
+            }
+        });
     }
 }
